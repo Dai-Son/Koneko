6421e4045da58498154387c5ee91cb4397669c73
DaiSon <61372575+Dai-Son@users.noreply.github.com>
2022-11-02 00:50:08 +0800

    Create bot.py, Add member join/leave events

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='.w.',intents=intents)
+
+@bot.event
+async def on_ready():
+    print(">> Meow Helo <<")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(1037043949837221929)
+    await channel.send(f'{member} 加入了!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1037043949837221929)
+    await channel.send(f'{member} 離開了!')
+
+bot.run('TOKEN')
